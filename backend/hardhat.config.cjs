d44b7bc9bfd221e3a06a373f6ffdd93caad73415
Louis Perrenot <perrenotlouis1804@gmail.com>
2025-06-03 10:51:58 +0200

    Add deploy and config sepolia

--- backend/hardhat.config.cjs
+++ backend/hardhat.config.cjs
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/8c959fc873b34ff390f409ccc3329e8a",
+      accounts: ["621578d6c43c26d0dd7c074b4b7652fea699a6b3a719e45995310c3f504efb67"],
+    }
+  }
 };
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,19 +1,19 @@
 import hre from "hardhat";
 
 async function main() {
-    const Vote = await hre.ethers.getContractFactory("Voting");
-    const voteInstance = await Vote.deploy();
+    const Voting = await hre.ethers.getContractFactory("Voting");
+    const voteInstance = await Voting.deploy();
 
     await voteInstance.waitForDeployment();
     const [owner] = await hre.ethers.getSigners();
     console.log("Deploying contract with address: ", owner.address);
 
-    const contractAddress = await safeInstance.getAddress();
-    console.log("Safe contract deployed at address:", contractAddress);
-}
-
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-}
-);
+    const contractAddress = await voteInstance.getAddress();
+    console.log("Contract Deployed to Address:", contractAddress);
+  }
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
